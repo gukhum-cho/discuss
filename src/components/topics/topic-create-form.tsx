a3730ae25bf156fe9f49f18f7d88b6b7ca2df1a2
jaeson-outplay <jaeson@outplay.games>
2024-06-10 01:43:40 +0800

    added base of discuss app

---
+++ src/components/topics/topic-create-form.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useFormState } from 'react-dom'
+import {
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+} from "@nextui-org/react";
+import { createTopic } from "@/actions"
+import FormButton from "@/components/common/form-button";
+
+export default function TopicCreateForm () {
+    const [ formState, action ] = useFormState( createTopic, {
+        errors: {}
+    });
+
+    return <Popover placement="left">
+        <PopoverTrigger>
+            <Button color="primary">Create a Topic</Button>
+        </PopoverTrigger>
+        <PopoverContent>
+            <form action={ action }>
+                <div className="flex flex-col gap-4 p-4 w-80">
+                    <h3 className="text-bold">Create a Topic</h3>
+                    <Input 
+                        name='name' 
+                        label="Name" 
+                        labelPlacement="outside" 
+                        placeholder="Name"
+                        isInvalid={!!formState.errors.name}
+                        errorMessage={formState.errors.name?.join(', ')}
+                    />
+                    <Textarea 
+                        name='description'
+                        label="Description" 
+                        labelPlacement="outside" 
+                        placeholder="Description your topic"
+                        isInvalid={!!formState.errors.description}
+                        errorMessage={formState.errors.description?.join(', ')}
+                    />
+                    {formState.errors._form ? <div className="p-2 bg-red-200 border rounded-lg border-red-400" >{formState.errors?._form.join(', ')}</div>: null}
+
+                    <FormButton>Submit</FormButton>
+                </div>
+            </form>
+        </PopoverContent>
+    </Popover>
+}
